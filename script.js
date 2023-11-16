113c897ea51305a0bf33ed8502d0495387b43ff5
thienmpham <thienmpham@gmail.com>
2023-11-16 13:32:16 -0500

    Learning about event delegation
    I was going to put an event handler for each individual category for the nav bar but realised thats a really inefficient way to code. So, I googled and found out about event delegation.
    
    My main problem is having the img rotate unique to the target event and not have other images rotate.

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
+let topNavCategoryContainer = document.querySelector('.top-nav-category-container');
+
+topNavCategoryContainer.addEventListener('click', (event) => {
+    let btn = event.target.closest('button');
+
+    if( !btn) return; // return nothing if event.target is not inside any button
+    
+
+
+
+})
+
+let arrowMove = () => {
+
+}
+
+
 let arrowUp = () => {
     let button = document.querySelector('.top-nav-category-container');
-    let category1 = document.querySelector('#category1');
+    let category = document.querySelector('.category-image');
     let open = false;
-    
-    console.log(category1);
+    let sonyIcon = document.querySelector('.top-nav-sony-icon')
     
     button.addEventListener('click', () => {
-        if(open){
-            category1.className = 'category-image open';
+        if(open) {
+            category.className = 'category-image open';
+            sonyIcon.className = 'top-nav-sony-icon open';
         } else {
-            
-            category1.className = '.category-image';
+            category.className = '.category-image';
+            sonyIcon.className = 'top-nav-sony-icon';
+
         }
 
         open = !open;
