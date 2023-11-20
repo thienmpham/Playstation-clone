b2980c59ac74b7358b0a39b8c603110e731a400c
thienmpham <thienmpham@gmail.com>
2023-11-20 12:03:02 -0500

    Slowly getting closer
    I am now using classList.toggle to rotate the arrow svg. Right now I am stuck on how to rotate the arrow when the container item is clicked rather than just the svg.

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
-let topNavCategoryContainer = document.querySelector('.top-nav-category-container');
+let topNavCategoryItem = document.querySelector('.top-nav-category-item');
+let topNavCategoryContainer = document.querySelector('.top-nav-category-container')
 
 topNavCategoryContainer.addEventListener('click', (event) => {
-    let btn = event.target.closest('button');
-
-    if( !btn) return; // return nothing if event.target is not inside any button
+    let btnImage = event.target.closest('.category-image');
+    btnImage.classList.toggle('category-image-open'); 
+    // btnImage.classList.toggle('top-nav-sony-icon-open');
     
-
-
-
+    
+    
 })
 
-let arrowMove = () => {
 
-}
+// let moveArrow = () => {
+//     let category = document.querySelector('.category-image');
+//     let sonyIcon = document.querySelector('.top-nav-sony-icon');
+//     let open = false;
 
+//     if(open) {
+//         category.className = 'category-image open';
+//         sonyIcon.className = 'top-nav-sony-icon open';
+//     } else {
+//         category.className = 'category-image';
+//         sonyIcon.className = 'top-nav-sony-icon';
 
-let arrowUp = () => {
-    let button = document.querySelector('.top-nav-category-container');
-    let category = document.querySelector('.category-image');
-    let open = false;
-    let sonyIcon = document.querySelector('.top-nav-sony-icon')
+//         }
+
+//         open = !open;
+// }
+
+// let arrowUp = () => {
+//     let button = document.querySelector('.top-nav-category-item');
+//     let category = document.querySelector('.category-image');
+//     let open = false;
+//     let sonyIcon = document.querySelector('.top-nav-sony-icon');
     
-    button.addEventListener('click', () => {
-        if(open) {
-            category.className = 'category-image open';
-            sonyIcon.className = 'top-nav-sony-icon open';
-        } else {
-            category.className = '.category-image';
-            sonyIcon.className = 'top-nav-sony-icon';
+//     button.addEventListener('click', () => {
+//         if(open) {
+//             category.className = 'category-image open';
+//             sonyIcon.className = 'top-nav-sony-icon open';
+//         } else {
+//             category.className = '.category-image';
+//             sonyIcon.className = 'top-nav-sony-icon';
 
-        }
+//         }
 
-        open = !open;
-    });
+//         open = !open;
+//     });
 
-}
+// }
 
-arrowUp();
+// arrowUp();
 
 
 
