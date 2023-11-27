9cc467894a2b25559263dbd36cdeab65a941d5f8
thienmpham <thienmpham@gmail.com>
2023-11-27 12:00:55 -0500

    Success!!! Event delegation!!
    I got the event delegation for the category buttons to work so that one menu at a time will be open. I learned about classList methods and also using events. Onto now the drop down menus for each category!

--- script.js
+++ script.js
@@ -4,33 +4,47 @@ let topNavCategoryContainer = document.querySelector('.top-nav-category-containe
 console.log(topNavCategoryContainer.childElementCount)
 
 
-for( let i = 0; i < topNavCategoryContainer.childElementCount; i++) {
+for( let i = 0; i <= topNavCategoryContainer.childElementCount; i++) {
 topNavCategoryContainer.addEventListener('click', (event) => {
     
     let btnImage = event.target.closest('button').querySelector('.category-image');
+    let openImage = document.querySelector('.category-image-open');
 
     if (btnImage.classList.contains('category-image-open')) {
         btnImage.classList.remove('category-image-open');
-        console.log('works!');
+
     }
     else {
+        try {
+        // If an element is already open, close it
+            openImage.classList.remove('category-image-open');
+            console.log(category-image-open);
+        }
+        catch (error){
+        }
+        // Add .category-image-open if btnImgae does not contain .category-image-open
         btnImage.classList.add('category-image-open'); 
     }
 })
+
 }
 
 
 
-let moveSonyIcon = () => {
+let iconEffect = () => {
     let button = document.querySelector('.top-nav-category-container');
+    let item = document.querySelector('.top-nav-category-item');
     let open = false;
     let sonyIcon = document.querySelector('.top-nav-sony-icon');
     
     button.addEventListener('click', () => {
-        if(open) {
+        if(!open) {
             sonyIcon.className = 'top-nav-sony-icon open';
+            button.classList.add('top-nav-category-fontsize')
+
         } else {
             sonyIcon.className = 'top-nav-sony-icon';
+            button.classList.remove('top-nav-category-fontsize')
         }
 
         open = !open;
@@ -38,6 +52,7 @@ let moveSonyIcon = () => {
 
 }
 
- moveSonyIcon();
+ iconEffect();
+
 
 
