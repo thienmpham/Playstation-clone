7d4468ea3eb3d6e4217f253c77ea4b9aad9cb0d6
thienmpham <thienmpham@gmail.com>
2023-12-05 11:06:28 -0500

    Finished up the css and blue text js
    I got the blue text toggle to work in my js code and finished up having the dropdown menu being position absolute.
    
    Next is the js code for the drop down menu and using opacity rather than display to get an animation for each category button click

--- script.js
+++ script.js
@@ -9,20 +9,25 @@ topNavCategoryContainer.addEventListener('click', (event) => {
     
     let btnImage = event.target.closest('button').querySelector('.category-image');
     let openImage = document.querySelector('.category-image-open');
-
+    let btnText = event.target.closest('button').querySelector('.category-name');
+    let blueText = document.querySelector('.top-nav-category-blue');
+    
     if (btnImage.classList.contains('category-image-open')) {
         btnImage.classList.remove('category-image-open');
+        btnText.classList.remove('top-nav-category-blue');
 
     }
     else {
         try {
         // If an element is already open, close it
             openImage.classList.remove('category-image-open');
+            blueText.classList.remove('top-nav-category-blue');
         }
         catch (error){
         }
         // Add .category-image-open if btnImgae does not contain .category-image-open
         btnImage.classList.add('category-image-open'); 
+        btnText.classList.add('top-nav-category-blue');
     }
 })
 
@@ -32,24 +37,21 @@ topNavCategoryContainer.addEventListener('click', (event) => {
 for( let i = 0; i <= topNavCategoryContainer.childElementCount; i++) {
     let iconEffect = () => {
         let button = document.querySelector('.top-nav-category-container');
-        let item = document.querySelector('.top-nav-category-item');
-        let open = false;
         let sonyIcon = document.querySelector('.top-nav-sony-icon');
 
         button.addEventListener('click', (event) => {
             let btnImage = event.target.closest('button').querySelector('.category-image');
-            let btnText = event.target.closest('button').querySelector('.category-name');
+        
 
             if(btnImage.classList.contains('category-image-open')) {
                 sonyIcon.className = 'top-nav-sony-icon open';
                 button.classList.add('top-nav-category-fontsize');
-                btnText.classList.add('top-nav-category-blue')
+                
 
             } else {
                 sonyIcon.className = 'top-nav-sony-icon';
                 button.classList.remove('top-nav-category-fontsize');
-                btnText.classList.remove('top-nav-category-blue')
-                
+            
             }
         })
 
