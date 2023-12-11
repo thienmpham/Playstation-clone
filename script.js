ac0cc034a7c41f00391ab1ae7b0a425931660903
thienmpham <thienmpham@gmail.com>
2023-12-11 12:00:55 -0500

    Making my code less messy
    I condensed my code into a singular listen event and now I am trying to figure out the sony icon toggle size.

--- script.js
+++ script.js
@@ -1,65 +1,33 @@
 let topNavCategoryItem = document.querySelector('.top-nav-category-item');
 let topNavCategoryContainer = document.querySelector('.top-nav-category-container');
 
-console.log(topNavCategoryContainer.childElementCount)
 
-
-for( let i = 0; i <= topNavCategoryContainer.childElementCount; i++) {
-document.addEventListener('click', (event) => {
-    
-    let btnImage = event.target.closest('button').querySelector('.category-image');
-    let openImage = document.querySelector('.category-image-open');
-    let btnText = event.target.closest('button').querySelector('.category-name');
-    let blueText = document.querySelector('.top-nav-category-blue');
-    
-    if (btnImage.classList.contains('category-image-open')) {
-        btnImage.classList.remove('category-image-open');
-        btnText.classList.remove('top-nav-category-blue');
-
-    }
-    else {
-        try {
-        // If an element is already open, close it
-            openImage.classList.remove('category-image-open');
-            blueText.classList.remove('top-nav-category-blue');
-        }
-        catch (error){
-        }
-        // Add .category-image-open if btnImgae does not contain .category-image-open
-        btnImage.classList.add('category-image-open'); 
-        btnText.classList.add('top-nav-category-blue');
-    }
-})
-
-}
-
-
-for ( let i = 0; i <= topNavCategoryContainer.childElementCount; i++) {
-    let iconEffect = () => {
-        // let button = document.querySelector('.top-nav-category-button');
+// for ( let i = 0; i <= topNavCategoryContainer.childElementCount; i++) {
+//     let iconEffect = () => {
+//         // let button = document.querySelector('.top-nav-category-button');
         
-        let sonyIcon = document.querySelector('.top-nav-sony-icon');
+//         let sonyIcon = document.querySelector('.top-nav-sony-icon');
 
-        document.addEventListener('click', (event) => {
-            let btnImage = event.target.closest('button').querySelector('.category-image');
-            let button = event.target.matches('top-nav-category-button');
+//         document.addEventListener('click', (event) => {
+//             let btnImage = event.target.closest('button').querySelector('.category-image');
+//             let button = event.target.matches('top-nav-category-button');
         
 
-            if(btnImage.classList.contains('category-image-open')) {
-                sonyIcon.className = 'top-nav-sony-icon open';
-                button.classList.add('top-nav-category-fontsize');
+//             if(btnImage.classList.contains('category-image-open')) {
+//                 sonyIcon.className = 'top-nav-sony-icon open';
+//                 button.classList.add('top-nav-category-fontsize');
                 
 
-            } else {
-                sonyIcon.className = 'top-nav-sony-icon';
-                button.classList.remove('top-nav-category-fontsize');
+//             } else {
+//                 sonyIcon.className = 'top-nav-sony-icon';
+//                 button.classList.remove('top-nav-category-fontsize');
             
-            }
-        })
+//             }
+//         })
 
-    }
-iconEffect();
-}
+//     }
+// iconEffect();
+// }
 
 
 
@@ -68,18 +36,35 @@ document.addEventListener('click', e => {
     const isDropdownButton = e.target.matches('[data-dropdown-button]');
     
     // If the click is not a dropdown button and is inside a dropdown menu then do nothing 
-    if (!isDropdownButton && e.target.closest('[]') !=null) return
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') !=null) return
 
     let currentDropdown;
+    let sonyIcon = document.querySelector('.top-nav-sony-icon');
+
     if(isDropdownButton) {
+       // Closes dropdown menu and also toggle adds active class to any children of data-dropdown
        currentDropdown = e.target.closest('[data-dropdown]');
        currentDropdown.classList.toggle('active');
-       console.log('click!');
+
+       // Toggle size of sony Icon 
+       
+    }
+
+    if (isDropdownButton && !sonyIcon.classList.contains('active')) {
+        sonyIcon.classList.add('active');
+    }
+    else if (sonyIcon.classList.contains('active')){ 
+        sonyIcon.classList.remove('active');
     }
     
+  
+    
+    // Closes all of dropdowns except current one 
     document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
         if (dropdown === currentDropdown) return
         dropdown.classList.remove('active');
+        
+        
     })
 
 })
