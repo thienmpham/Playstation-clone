3d08e5fe63f5b607990e54f680b14c69400882a5
thienmpham <thienmpham@gmail.com>
2023-12-13 11:43:52 -0500

    Almost done with nav bar
    I am finishing up having the dropdown images animation and after that I will be done with the nav bar!

--- script.js
+++ script.js
@@ -2,52 +2,18 @@ let topNavCategoryItem = document.querySelector('.top-nav-category-item');
 let topNavCategoryContainer = document.querySelector('.top-nav-category-container');
 
 
-// for ( let i = 0; i <= topNavCategoryContainer.childElementCount; i++) {
-//     let iconEffect = () => {
-//         // let button = document.querySelector('.top-nav-category-button');
-        
-//         let sonyIcon = document.querySelector('.top-nav-sony-icon');
-
-//         document.addEventListener('click', (event) => {
-//             let btnImage = event.target.closest('button').querySelector('.category-image');
-//             let button = event.target.matches('top-nav-category-button');
-        
-
-//             if(btnImage.classList.contains('category-image-open')) {
-//                 sonyIcon.className = 'top-nav-sony-icon open';
-//                 button.classList.add('top-nav-category-fontsize');
-                
-
-//             } else {
-//                 sonyIcon.className = 'top-nav-sony-icon';
-//                 button.classList.remove('top-nav-category-fontsize');
-            
-//             }
-//         })
-
-//     }
-// iconEffect();
-// }
-
-
-
-
 document.addEventListener('click', e => {
     const isDropdownButton = e.target.matches('[data-dropdown-button]');
     
     // If the click is not a dropdown button and is inside a dropdown menu then do nothing 
     if (!isDropdownButton && e.target.closest('[data-dropdown]') !=null) return
 
+    let dropdownImages
     let currentDropdown;
-    let sonyIcon = document.querySelector('.top-nav-sony-icon');
     if(isDropdownButton) {
        // Closes dropdown menu and also toggle adds active class to any children of data-dropdown
        currentDropdown = e.target.closest('[data-dropdown]');
        currentDropdown.classList.toggle('active');
-
-    
-       sonyIcon.classList.add('active');
-       
 
     }
     
@@ -57,10 +23,33 @@ document.addEventListener('click', e => {
         if (dropdown === currentDropdown) return
         dropdown.classList.remove('active');
         
-        if(!dropdown.classList.contains('active')) {
-            sonyIcon.classList.remove('active');
-        }
-        
     })
 })
 
+document.addEventListener('click', e => {
+    let sonyIcon = document.querySelector('.top-nav-sony-icon');
+    let isButton = e.target.closest('.top-nav-category-item');
+    let name = document.querySelectorAll('.category-name');
+    let image = document.querySelectorAll('.category-image')
+ 
+        if (isButton.classList.contains('active')){
+            sonyIcon.classList.add('active');
+            name.forEach(names => {
+                names.classList.add('active');
+            })
+
+            image.forEach(images => {
+                images.classList.add('resize');
+            })
+        }  
+        else {
+            sonyIcon.classList.remove('active');
+            name.forEach( names => {
+                names.classList.remove('active');
+            })
+
+            image.forEach (images => {
+                images.classList.remove('resize');
+            })
+        }
+})
