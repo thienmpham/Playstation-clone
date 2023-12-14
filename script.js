0b4b52fb64cb850bd43e3ca033313b05df2768a9
thienmpham <thienmpham@gmail.com>
2023-12-14 11:05:27 -0500

    Onto the sign in and search button
    I have finished the dropdown menu thus far with using a lot of for each methods and using selectors for getting the children of childrens!
    A very handy method if I say so myself

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ document.addEventListener('click', e => {
     // If the click is not a dropdown button and is inside a dropdown menu then do nothing 
     if (!isDropdownButton && e.target.closest('[data-dropdown]') !=null) return
 
-    let dropdownImages
     let currentDropdown;
     if(isDropdownButton) {
        // Closes dropdown menu and also toggle adds active class to any children of data-dropdown
        currentDropdown = e.target.closest('[data-dropdown]');
        currentDropdown.classList.toggle('active');
+   
 
     }
     
@@ -31,7 +31,8 @@ document.addEventListener('click', e => {
     let isButton = e.target.closest('.top-nav-category-item');
     let name = document.querySelectorAll('.category-name');
     let image = document.querySelectorAll('.category-image')
- 
+    let dropdownImages = e.target.closest('.dropdown-images');
+
         if (isButton.classList.contains('active')){
             sonyIcon.classList.add('active');
             name.forEach(names => {
@@ -41,6 +42,11 @@ document.addEventListener('click', e => {
             image.forEach(images => {
                 images.classList.add('resize');
             })
+
+            // dropdownImages.forEach(dropImages => {
+            // dropImages.classList.add('active');
+            // })
+        
         }  
         else {
             sonyIcon.classList.remove('active');
